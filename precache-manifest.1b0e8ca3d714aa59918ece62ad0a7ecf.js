f28e6346774cc3bbf4b4acf09c40f10fc02f5c04
Cosmina <cosmina.palade@gmail.com>
2019-09-13 19:08:28 +0300

    Updates

---
+++ precache-manifest.1b0e8ca3d714aa59918ece62ad0a7ecf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25da31fc41b143c7e26667e5a13d9193",
+    "url": "/React-Projects/index.html"
+  },
+  {
+    "revision": "d2a351fad8e4a3a237d6",
+    "url": "/React-Projects/static/css/main.f6ee5688.chunk.css"
+  },
+  {
+    "revision": "c0e0ca6c55b178647659",
+    "url": "/React-Projects/static/js/2.15c91c0a.chunk.js"
+  },
+  {
+    "revision": "d2a351fad8e4a3a237d6",
+    "url": "/React-Projects/static/js/main.9b078b0c.chunk.js"
+  },
+  {
+    "revision": "a1bce1119595e595d1b1",
+    "url": "/React-Projects/static/js/runtime~main.c7ee9447.js"
+  }
+]);
